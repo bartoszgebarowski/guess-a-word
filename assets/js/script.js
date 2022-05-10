3b44aae1bac1f297b4a8056ba6422629fae18696
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2022-05-10 15:55:17 +0200

    add logic that will display user words on the game board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+// Global parameters 
+
+let rowCounter = 1;
+let userWord = [];
+
 // Wait for the DOM finish loading 
 // Get the buttons elements and add event listeners
 
@@ -6,16 +11,27 @@ document.addEventListener('DOMContentLoaded', function() {
   for (let letterButton of letterButtons) {
     letterButton.addEventListener('click', function() {
       console.log(`${letterButton.textContent} pressed`);
-    })
+      userWord.push(letterButton.innerHTML);
+    });
   }
   // Enter button
   let enterButton = document.getElementById('enter-button');
   enterButton.addEventListener('click', function() {
     alert('Enter pressed');
-  })
+    // Currently active row on the board
+    let activeRow = document.querySelector(`#row-${rowCounter}`);
+    // Add user word into the board
+    for(i = 0; i < activeRow.children.length; i++) {
+      let letterInsert = activeRow.children[i];
+      letterInsert.innerHTML = userWord[i];
+    }
+    // Clear the user word and change the active row
+    userWord = [];
+    rowCounter = rowCounter + 1;
+  });
   // Delete Button
   let deleteButton = document.getElementById('delete-button');
   deleteButton.addEventListener('click', function() {
     alert('Delete pressed');
-  })
-})
+  });
+});
