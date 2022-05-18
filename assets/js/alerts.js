e927e990a577d34a6c2e547adc3b1326d3263886
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2022-05-18 20:43:04 +0200

    add statistics functionality and improve code indentation

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -31,13 +31,13 @@ function rulesSwalAlert() {
 
   Swal.fire({
     customClass: {
-      htmlContainer: 'html-container-height',
+      htmlContainer: "html-container-height",
     },
-    icon: 'info',
-    width: '1000px',
+    icon: "info",
+    width: "1000px",
     html: rulesHTML,
-    confirmButtonColor: '#3085d6',
-    confirmButtonText: 'Ok, got it!',
+    confirmButtonColor: "#3085d6",
+    confirmButtonText: "Ok, got it!",
     allowOutsideClick: false,
     allowEscapeKey: false,
   });
@@ -45,16 +45,15 @@ function rulesSwalAlert() {
 
 // This function will bring rules popup
 
-
 // This function will bring up the SweetAlert2 popup with desired text
 
 function swalError(text) {
   Swal.fire({
-    icon: 'error',
-    title: 'Error',
+    icon: "error",
+    title: "Error",
     text: `${text}`,
     allowOutsideClick: false,
-    confirmButtonColor: '#FF0000',
+    confirmButtonColor: "#FF0000",
     allowEscapeKey: false,
   });
 }
@@ -63,36 +62,69 @@ function swalError(text) {
 
 function swalConfirm(text) {
   Swal.fire({
-    icon: 'success',
-    title: 'Good Job',
+    icon: "success",
+    title: "Good Job",
     text: `${text}`,
     allowOutsideClick: false,
-    confirmButtonColor: '#1DB954',
+    confirmButtonColor: "#1DB954",
     allowEscapeKey: false,
   });
 }
 
 function swalWarning(text) {
   Swal.fire({
-    icon: 'warning',
-    title: 'Game Over',
+    icon: "warning",
+    title: "Game Over",
     text: `${text}`,
     allowOutsideClick: false,
-    confirmButtonColor: '#facea8',
-    confirmButtonText: 'Try again',
+    confirmButtonColor: "#facea8",
+    confirmButtonText: "Try again",
     allowEscapeKey: false,
   });
 }
 
-function swalStatistics() {
-  let statisticsHTML =`
- <p>Placeholder</p>`;
+//////
+
+function swalStatistics(sortedPlayers) {
+  let statisticsHTML = `
+    <table style="width: 100%">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Score</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody id="table-data"></tbody>
+    </table>
+  </div>
+  `;
   Swal.fire({
-    title: 'Statistics',
+    title: "Statistics",
     html: statisticsHTML,
     allowOutsideClick: false,
-    confirmButtonColor: '#1DB954',
-    confirmButtonText: 'OK',
+    confirmButtonColor: "#1DB954",
+    confirmButtonText: "OK",
     allowEscapeKey: false,
   });
+
+  createTableFromData(sortedPlayers);
+}
+
+// function that will create table body from input data
+function createTableFromData(data) {
+  let tableBody = document.getElementById("table-data");
+  for (let player of data) {
+    tr = document.createElement("tr");
+    tdName = document.createElement("td");
+    tdName.innerHTML = player.name;
+    tdScore = document.createElement("td");
+    tdScore.innerHTML = player.score;
+    tdTime = document.createElement("td");
+    tdTime.innerHTML = player.gameTime;
+    tr.appendChild(tdName);
+    tr.appendChild(tdScore);
+    tr.appendChild(tdTime);
+    tableBody.appendChild(tr);
+  }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,60 @@
-// Global parameters 
+// Global parameters
 
 let rowCounter = 1;
 let userWord = [];
-let puzzleWordPool = ['WORLD', 'SMELL', 'PRIDE'];
+let puzzleWordPool = ["WORLD", "SMELL", "PRIDE"];
 let puzzleWord = Array.from(puzzleWordPool[0]);
 let playerId;
+let scoreBoard = [];
 
 //Function that will change the color of tile in the game board depending on the user input and puzzle word
 
 function tileColor(tile, color) {
-    tile.classList.add(`${color}-button`);
+  tile.classList.add(`${color}-button`);
 }
-
 
 // Function that will change background color of button to red, if the letter does not appear in the word from the puzzle
 
 function buttonToRed(wrongLetter, correctLetter, puzzleArray) {
-  let letterElement = document.getElementById(`letter-${wrongLetter.toLowerCase()}`);
-  if(puzzleArray.includes(wrongLetter) === false) {
-    letterElement.classList.add('red-button');
+  let letterElement = document.getElementById(
+    `letter-${wrongLetter.toLowerCase()}`
+  );
+  if (puzzleArray.includes(wrongLetter) === false) {
+    letterElement.classList.add("red-button");
   }
 }
 
 // Function that checks if letter is correct
-function checkForCorrectLetter(correctAnswer, userAnswer,) {
+function checkForCorrectLetter(correctAnswer, userAnswer) {
   let activeRow = document.querySelector(`#row-${rowCounter}`);
-  for(i = 0; i < userAnswer.length; i++) {
+  for (i = 0; i < userAnswer.length; i++) {
     let guessedLetter = userAnswer[i];
     let correctLetter = correctAnswer[i];
-    if(guessedLetter === correctLetter) {
+    if (guessedLetter === correctLetter) {
       // Mark correct letters green/yellow and change them to number(disable letter from further search)
-      tileColor(activeRow.children[i], 'green');
-      correctAnswer[i] = 0; 
+      tileColor(activeRow.children[i], "green");
+      correctAnswer[i] = 0;
     } else {
       buttonToRed(guessedLetter, correctLetter, puzzleWord);
     }
-}
+  }
 }
 
-// Clear Board 
+// Clear Board
 
 function clearBoard() {
-  let puzzleRows = document.querySelectorAll('.puzzle-area-row-flex');
+  let puzzleRows = document.querySelectorAll(".puzzle-area-row-flex");
   for (let tiles of puzzleRows) {
     let allTiles = tiles.children;
     for (let tile of allTiles) {
-      tile.innerHTML = '';
-      tile.classList.remove('yellow-button');
-      tile.classList.remove('green-button');
+      tile.innerHTML = "";
+      tile.classList.remove("yellow-button");
+      tile.classList.remove("green-button");
     }
   }
-  let keyboardButtons = document.querySelectorAll('.letter-button');
+  let keyboardButtons = document.querySelectorAll(".letter-button");
   for (let button of keyboardButtons) {
-    button.classList.remove('red-button');
+    button.classList.remove("red-button");
   }
 }
 
@@ -60,17 +62,19 @@ function clearBoard() {
 
 function winConditionCheck(correctAnswer) {
   let firstLetter = correctAnswer[0];
-    let fourLetters = correctAnswer.slice(1,5);
-    let fourLettersJoin = fourLetters.join('');
-    let fourLettersJoinLowerCase = fourLettersJoin.toLowerCase();
-    let answerToDisplay = firstLetter + fourLettersJoinLowerCase;
-    swalConfirm(`That is correct ! The puzzle word was ${answerToDisplay}`);
-    console.log(playerId);
-    player = getPlayerById(playerId);
-    console.log(player);
-    player.score++;
-    updatePlayer(player);
-    clearBoard();
+  let fourLetters = correctAnswer.slice(1, 5);
+  let fourLettersJoin = fourLetters.join("");
+  let fourLettersJoinLowerCase = fourLettersJoin.toLowerCase();
+  let answerToDisplay = firstLetter + fourLettersJoinLowerCase;
+  swalConfirm(`That is correct ! The puzzle word was ${answerToDisplay}`);
+  console.log(playerId);
+  player = getPlayerById(playerId);
+  console.log(player);
+  player.score++;
+  player.endTime = new Date().getTime();
+  player.gameTime = (player.endTime - player.startTime) / 1000;
+  updatePlayer(player);
+  clearBoard();
 }
 
 // Game over condition check
@@ -78,16 +82,18 @@ function winConditionCheck(correctAnswer) {
 function gameOverConditionCheck(userAnswer, correctAnswer) {
   if (userAnswer.toString() !== correctAnswer.toString()) {
     let firstLetter = correctAnswer[0];
-    let fourLetters = correctAnswer.slice(1,5);
-    let fourLettersJoin = fourLetters.join('');
+    let fourLetters = correctAnswer.slice(1, 5);
+    let fourLettersJoin = fourLetters.join("");
     let fourLettersJoinLowerCase = fourLettersJoin.toLowerCase();
     let answerToDisplay = firstLetter + fourLettersJoinLowerCase;
     player = getPlayerById(playerId);
     console.log(player);
     player.endTime = new Date().getTime();
-    player.gameTime = (player.endTime - player.startTime)/1000;
+    player.gameTime = (player.endTime - player.startTime) / 1000;
     updatePlayer(player);
-    swalWarning(`You have used all of your tries. The correct answer was ${answerToDisplay}`);
+    swalWarning(
+      `You have used all of your tries. The correct answer was ${answerToDisplay}`
+    );
     clearBoard();
   }
 }
@@ -96,23 +102,24 @@ function gameOverConditionCheck(userAnswer, correctAnswer) {
 
 function checkForLetterPlacement(correctAnswer, userAnswer) {
   let activeRow = document.querySelector(`#row-${rowCounter}`);
-  for(i = 0; i < userAnswer.length; i++) {
+  for (i = 0; i < userAnswer.length; i++) {
     let guessedLetter = userAnswer[i];
     let letterIndex = correctAnswer.indexOf(guessedLetter);
-      if(letterIndex !== -1) {
-        // // Mark correct letter but not in their place and change them to number(disable letter from further search)
-        tileColor(activeRow.children[i], 'yellow');
-        correctAnswer[letterIndex] = 0;
-}}}
+    if (letterIndex !== -1) {
+      // // Mark correct letter but not in their place and change them to number(disable letter from further search)
+      tileColor(activeRow.children[i], "yellow");
+      correctAnswer[letterIndex] = 0;
+    }
+  }
+}
 
-
-// Function that will check the answer 
+// Function that will check the answer
 
 function checkAnswer(correctAnswer, userAnswer) {
   let activeRow = document.querySelector(`#row-${rowCounter}`);
-  if(correctAnswer.toString() === userAnswer.toString()) {
-    for(let tile of activeRow.children) {
-      tileColor(tile, 'green');
+  if (correctAnswer.toString() === userAnswer.toString()) {
+    for (let tile of activeRow.children) {
+      tileColor(tile, "green");
     }
     winConditionCheck(puzzleWord);
     rowCounter = 0;
@@ -133,108 +140,110 @@ function insertValue(rowNumber, index, value) {
 // Function that validate the correct number of letters
 
 function pushLetterValidation(letterButton, userWord) {
-  if(userWord.length < 5) {
-    let letter = letterButton.innerHTML; 
+  if (userWord.length < 5) {
+    let letter = letterButton.innerHTML;
     userWord.push(letter);
     let letterIndex = userWord.length - 1;
     insertValue(rowCounter, letterIndex, letter);
   } else {
-    swalError('Too many letters. To continue, please, confirm your answer.');
+    swalError("Too many letters. To continue, please, confirm your answer.");
   }
 }
 
 // Function that submit word to row
 
 function submitAnswer(rowNumber, wordArray) {
-    // Currently active row on the board
-    let activeRow = document.querySelector(`#row-${rowNumber}`);
-    // Add user word into the board
-    for(i = 0; i < activeRow.children.length; i++) {
-      let letterInsert = activeRow.children[i];
-      letterInsert.innerHTML = wordArray[i];
-    }
+  // Currently active row on the board
+  let activeRow = document.querySelector(`#row-${rowNumber}`);
+  // Add user word into the board
+  for (i = 0; i < activeRow.children.length; i++) {
+    let letterInsert = activeRow.children[i];
+    letterInsert.innerHTML = wordArray[i];
+  }
 }
-
 
 function requestToDictionaryAPI(word) {
   var request = new XMLHttpRequest();
-  request.open('GET', `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, false);  // `false` makes the request synchronous
+  request.open(
+    "GET",
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+    false
+  ); // `false` makes the request synchronous
   request.send(null);
-  return request.status
+  return request.status;
 }
 
 function isValidWord(word) {
- let status = requestToDictionaryAPI(word.join(''));
- if(status === 200) {
-  return true;
- }
- return false;
+  let status = requestToDictionaryAPI(word.join(""));
+  if (status === 200) {
+    return true;
+  }
+  return false;
 }
 
 // Function that will get the enter and delete buttons elements and add event listeners
 
 function initiateButtonsActions() {
-  let letterButtons = document.querySelectorAll('.letter-button');
+  let letterButtons = document.querySelectorAll(".letter-button");
   for (let letterButton of letterButtons) {
-    letterButton.addEventListener('click', function() {
+    letterButton.addEventListener("click", function () {
       console.log(`${letterButton.textContent} pressed`);
       pushLetterValidation(letterButton, userWord);
     });
   }
   // Enter button
-  let enterButton = document.getElementById('enter-button');
-  enterButton.addEventListener('click', function() {
+  let enterButton = document.getElementById("enter-button");
+  enterButton.addEventListener("click", function () {
     // Words must be 5 characters long
-    if(userWord.length === 5) {
-      if(isValidWord(userWord)) {
+    if (userWord.length === 5) {
+      if (isValidWord(userWord)) {
         submitAnswer(rowCounter, userWord);
         checkAnswer(puzzleWord.slice(), userWord);
         // Check for game over condition
-        if(rowCounter > 5) {
+        if (rowCounter > 5) {
           gameOverConditionCheck(userWord, puzzleWord);
           rowCounter = 0;
         }
         userWord = [];
         rowCounter = rowCounter + 1;
       } else {
-        swalError('It must be a valid word')
+        swalError("It must be a valid word");
       }
     } else {
-      swalError('Word must be 5 characters long.');
+      swalError("Word must be 5 characters long.");
     }
   });
   // Delete Button
-  let deleteButton = document.getElementById('delete-button');
-  deleteButton.addEventListener('click', function() {
-    if(userWord.length) {
+  let deleteButton = document.getElementById("delete-button");
+  deleteButton.addEventListener("click", function () {
+    if (userWord.length) {
       let letterIndex = userWord.length - 1;
-      insertValue(rowCounter, letterIndex, '');
+      insertValue(rowCounter, letterIndex, "");
       userWord.pop();
     } else {
-      swalError('There is nothing to remove');
+      swalError("There is nothing to remove");
     }
   });
 }
-
 
 // Function that will display SweetAlert, capture user input and push it to local storage
 
 function swalInput(id) {
   Swal.fire({
-    title: 'Choose your username',
+    title: "Choose your username",
     html: `<input type="text" id="username" class="swal2-input" placeholder="Username">`,
     confirmButtonText: "I'm, ready",
-    confirmButtonColor: '#1DB954',
+    confirmButtonColor: "#1DB954",
     focusConfirm: false,
     allowOutsideClick: false,
     allowEscapeKey: false,
     preConfirm: () => {
-      let input = Swal.getPopup().querySelector('#username').value;
+      let input = Swal.getPopup().querySelector("#username").value;
       if (!input) {
         Swal.showValidationMessage(`Please enter your username`);
       }
       return input;
-    }
+    },
   }).then((input) => {
     players = getPlayers();
     player = getPlayerById(id);
@@ -244,25 +253,26 @@ function swalInput(id) {
   });
 }
 
-//  Wait for the DOM finish loading 
+//  Wait for the DOM finish loading
 // Get the rules and statistics element, add event listeners,  SweetAlert modals
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Rules
-  let rulesInfo = document.getElementById('rules');
-  rulesInfo.addEventListener('click', function() {
+
+  let rulesInfo = document.getElementById("rules");
+  rulesInfo.addEventListener("click", function () {
     rulesSwalAlert();
   });
-  // Stats 
-  let statistics = document.getElementById('statistics');
-  statistics.addEventListener('click', function() {
-    swalStatistics();
+  // Stats
+  let statistics = document.getElementById("statistics");
+  statistics.addEventListener("click", function () {
+    swalStatistics(getTopFivePlayers());
   });
 });
 
 //  Wait for the DOM finish loading , start the game
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Initiate local storage when one does not exist
   if (!localStorage.Players) {
     localStorage.Players = JSON.stringify([]);
@@ -278,12 +288,12 @@ document.addEventListener('DOMContentLoaded', function() {
 function createNewPlayer() {
   players = getPlayers();
   let lastId = 0;
-  if(players.length) {
-    lastId =  players.at(-1).id;
+  if (players.length) {
+    lastId = players.at(-1).id;
   }
   let newPlayer = {
     id: lastId + 1,
-    name: '',
+    name: "",
     score: 0,
     gameTime: 0,
   };
@@ -308,7 +318,9 @@ function savePlayer(newPlayer) {
 // Update player
 
 function updatePlayer(updatedPlayer) {
-  players = players.map(player => player.id !== updatedPlayer.id ? player : updatedPlayer);
+  players = players.map((player) =>
+    player.id !== updatedPlayer.id ? player : updatedPlayer
+  );
   savePlayers(players);
 }
 
@@ -322,9 +334,44 @@ function savePlayers(players) {
 
 function getPlayerById(id) {
   players = getPlayers();
-  for(let player of players) {
-    if(player.id == id) {
-         return player;
-        }
+  for (let player of players) {
+    if (player.id == id) {
+      return player;
+    }
   }
+}
+
+// Function that will filter out invalid entries
+
+function getLeaderBoardPlayers() {
+  let dataToSort = getPlayers();
+  let leaderBoardPlayers = [];
+  for (i = 0; i < dataToSort.length; i++) {
+    if (
+      dataToSort[i].name &&
+      dataToSort[i].gameTime !== 0 &&
+      dataToSort[i].name !== ""
+    ) {
+      leaderBoardPlayers.push(dataToSort[i]);
+    }
+  }
+  return leaderBoardPlayers;
+}
+
+getLeaderBoardPlayers();
+
+// Function that will sort players
+
+function sortPlayers(dataToSort) {
+  let dataSorted = dataToSort.sort(
+    (a, b) => a.name - b.name || b.score - a.score || a.gameTime - b.gameTime
+  );
+  return dataSorted;
+}
+
+// Return top five players
+
+function getTopFivePlayers() {
+  let sortedPlayers = sortPlayers(getLeaderBoardPlayers());
+  return sortedPlayers.slice(0, 5);
 }
